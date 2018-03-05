62042e0eadfe38866a6d9410c36cbce39f119a2e
dmitriy_ee <dmitriy_ee@hotmail.com>
2018-03-05 22:54:18 +0200

    Revert "add_database_cleaner"
    This reverts commit bec0200ead2661af8050f2563b9db619eebf03f2.

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -6,4 +6,15 @@ RSpec.feature "Account Creation",  :type => :feature do
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
 
+  def sign_up
+      visit new_user_registration_path
+
+      fill_in :user_email, :with => 'user@example.com'
+      fill_in :user_username, :with => 'asass'
+      fill_in :user_password, :with => 'secure!123#'
+      fill_in :user_password_confirmation, :with => 'secure!123#'
+      
+      click_button 'Sign up'
+  end
+
 end
